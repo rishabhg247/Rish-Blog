d07c4656934549eecb893ce4b9d57a0815ed662e
Rishabh Gupta <rg1224362@gmail.com>
2023-07-26 17:43:55 +0530

    updated server links

--- api/index.js
+++ api/index.js
@@ -3,8 +3,19 @@ import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
-
 const app = express();
+
+import path from "path";
+const _dirname=path.dirname("");
+const buildPath =path.join(_dirname,"../client/build");
+app.use(express.static(buildPath));
+app.get("/",(req,res)=>{
+    res.sendFile(
+        path.join(__dirname,'../client/build/index.html'),
+        (err)=>{if(err){res.status(500).send(err)}}
+    )
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
